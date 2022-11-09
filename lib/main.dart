d8924ddeba829c075fdcc0c58d3145dd9a722f16
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2022-11-10 00:13:12 +0200

    Add multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:anki/map_generator.dart';
+import 'package:anki/model/player.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'widgets/board.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => PlayerModel()),
+        ChangeNotifierProvider(
+            create: (context) => MapGenerator().generateRandomMap(10, 10)),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -33,11 +45,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var player = Provider.of<PlayerModel>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Board()
+      body: Column(
+        children: [
+          Flexible(
+            child: const Board(),
+            flex: 5,
+          ),
+          Flexible(
+            flex: 1,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          player.y = player.y - 1;
+                        },
+                        child: Text("U"))
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          player.x = player.x - 1;
+                        },
+                        child: Text("L")),
+                    ElevatedButton(
+                        onPressed: () {
+                          player.y = player.y + 1;
+                        },
+                        child: Text("D")),
+                    ElevatedButton(
+                        onPressed: () {
+                          player.x = player.x + 1;
+                        },
+                        child: Text("R")),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
