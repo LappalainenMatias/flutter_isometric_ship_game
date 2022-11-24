c421a0e298756336563b984a25e5c9fceb73f767
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2022-11-24 15:13:46 +0200

    Add more square types

--- lib/enum/square_type.dart
+++ lib/enum/square_type.dart
@@ -2,26 +2,35 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 enum SquareType {
-  mountain,
-  rock,
+  rockHigh,
+  rockMed,
+  rockLow,
   trees,
   grass,
   water,
+  wall,
+  sand,
 }
 
 extension SquareTypeExtension on SquareType {
   Color get color {
     switch (this) {
       case SquareType.grass:
-        return Colors.green;
+        return const Color.fromARGB(255, 8, 161, 12);
       case SquareType.trees:
-        return Colors.green;
-      case SquareType.mountain:
-        return Colors.grey;
-      case SquareType.rock:
-        return Colors.grey;
+        return const Color.fromARGB(255, 23, 129, 11);
+      case SquareType.rockHigh:
+        return const Color.fromARGB(255, 110, 110, 110);
+      case SquareType.rockMed:
+        return const Color.fromARGB(255, 130, 130, 130);
+      case SquareType.rockLow:
+        return const Color.fromARGB(255, 150, 150, 150);
       case SquareType.water:
         return Colors.blue;
+      case SquareType.sand:
+        return const Color.fromARGB(255, 194, 178, 128);
+      case SquareType.wall:
+        return Colors.black26;
       default:
         return Colors.black;
     }
@@ -29,6 +38,7 @@ extension SquareTypeExtension on SquareType {
 
   static SquareType get getRandomType {
     List<SquareType> values = SquareType.values;
+    values.remove(SquareType.wall);
     return values[Random().nextInt(values.length)];
   }
 
@@ -39,14 +49,16 @@ extension SquareTypeExtension on SquareType {
 
   bool isVisitable() {
     if (this == SquareType.water) return false;
+    if (this == SquareType.wall) return false;
     return true;
   }
 
   static SquareType getValueBasedOnHeight(double height) {
-    if (height > 0.72) return SquareType.mountain;
-    if (height > 0.55) return SquareType.rock;
-    if (height > 0.53) return SquareType.trees;
-    if (height > 0.4) return SquareType.grass;
+    if (height > 0.50) return SquareType.rockHigh;
+    if (height > 0.40) return SquareType.rockMed;
+    if (height > 0.30) return SquareType.rockLow;
+    if (height > -0.10) return SquareType.grass;
+    if (height > -0.15) return SquareType.sand;
     return SquareType.water;
   }
 }
