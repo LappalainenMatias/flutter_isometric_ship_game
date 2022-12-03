f33290b4a77dbe766534675ca55218d55f035339
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2022-12-03 19:25:18 +0200

    Use width and height for painting

--- lib/widget/board.dart
+++ lib/widget/board.dart
@@ -1,16 +1,14 @@
 import 'package:anki/custom_painter.dart';
-import 'package:anki/enum/item.dart';
-import 'package:anki/enum/square_visibility.dart';
 import 'package:anki/model/player.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../enemy.dart';
-import '../model/game_model.dart';
-import 'dart:math';
-import '../square.dart';
+import '../model/game.dart';
 
 class Board extends StatefulWidget {
-  const Board({super.key});
+  final double width;
+  final double height;
+
+  const Board({super.key, required this.width, required this.height});
 
   @override
   State<Board> createState() => _BoardState();
@@ -19,17 +17,15 @@ class Board extends StatefulWidget {
 class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: SizedBox(width: 201, child: _buildCustomPaint()));
+    return SizedBox(
+        width: widget.width, height: widget.height, child: _buildCustomPaint());
   }
 
   Widget _buildCustomPaint() {
-    var game = Provider.of<GameModel>(context, listen: false);
-    var player = Provider.of<PlayerModel>(context, listen: true);
+    Provider.of<PlayerModel>(context, listen: true);
     return CustomPaint(
-      size: const Size(201, 201),
-      painter: MapPainter(game),
+      size: Size(widget.width, widget.height),
+      painter: MapPainter(Provider.of<GameModel>(context, listen: false)),
     );
   }
 }
