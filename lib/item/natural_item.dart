998796acab07b8af8c0f3bada1908ccc6cafd6ce
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2022-12-10 11:47:32 +0200

    Create rocks

--- lib/item/natural_item.dart
+++ lib/item/natural_item.dart
@@ -5,6 +5,7 @@ import 'item.dart';
 
 /// Contains items like trees and rocks
 enum NaturalItem implements Item {
+  rock(Color.fromARGB(255, 128, 128, 128), "rock"),
   tree(Color.fromARGB(255, 25, 121, 29), "tree"),
   bush(Color.fromARGB(255, 80, 166, 60), "bush");
 
@@ -19,9 +20,8 @@ extension NaturalItemExtension on NaturalItem {
   static NaturalItem? getNaturalItem(SquareType type) {
     List<NaturalItem> supported = type.supportedNaturalItems;
     if (supported.isEmpty) return null;
-    double probability = 0.05;
     Random random = Random();
-    if (random.nextDouble() > probability) return null;
+    if (random.nextDouble() > 0.05) return null;
     return supported.elementAt(random.nextInt(supported.length));
   }
 }
