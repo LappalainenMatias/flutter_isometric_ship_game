086c8fa91c5fa492ba661cb6b10b610981aaef1c
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2022-12-16 18:43:49 +0200

    Reduce functionality from game class

--- lib/game.dart
+++ lib/game.dart
@@ -1,26 +1,13 @@
 import 'package:anki/character/character_manager.dart';
-import 'package:anki/map/square_visibility.dart';
 import 'package:anki/map/map.dart';
-import 'package:anki/character/player.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:math';
-
-import 'character/enemy.dart';
-import 'map/square_type.dart';
-import 'map/square.dart';
 
 class GameModel extends ChangeNotifier {
   MapModel map;
-  PlayerModel player;
-  List<Enemy> enemies;
   bool paused = true;
-
-  /// Notice that vision must be odd so that player can be at the center of vision
-  int _vision = 50;
-  final double _zoomMultiplier = 1.5;
   final CharacterManager _characterManager;
 
-  GameModel(this.map, this.player, this.enemies, this._characterManager);
+  GameModel(this.map, this._characterManager);
 
   void start() {
     paused = false;
@@ -33,66 +20,4 @@ class GameModel extends ChangeNotifier {
     _characterManager.pause();
     notifyListeners();
   }
-
-  void _updateVision(int newVision) {
-    if (newVision < 3) {
-      _vision = 3;
-      notifyListeners();
-    } else {
-      if (newVision.isEven) {
-        newVision += 1;
-      }
-      _vision = newVision;
-      map.notifyListeners();
-      notifyListeners();
-    }
-  }
-
-  void zoomIn() {
-    _updateVision((_vision / _zoomMultiplier).round());
-  }
-
-  void zoomOut() {
-    _updateVision((_vision * _zoomMultiplier).round());
-  }
-
-  /// Resolution is the widgets resolution.
-  /// We use resolution so that we do not return unnecessary large amount of squares.
-  List<List<Square>> getSquaresInVision(Size resolution) {
-    int halfVision = (vision / 2).ceil();
-    List<List<Square>> squares = _getSquaresWithMaxResolution(
-        Point(map.playerCoordinate.x - halfVision,
-            map.playerCoordinate.y - halfVision),
-        Point(map.playerCoordinate.x + halfVision,
-            map.playerCoordinate.y + halfVision),
-        resolution);
-    return squares;
-  }
-
-  /// returns all the squares which are in the are defined by top left and bottom right corner
-  List<List<Square>> _getSquaresWithMaxResolution(
-      Point topLeft, Point bottomRight, Size resolution) {
-    /// Todo maybe we could just return list of points. This could be faster
-    List<List<Square>> squares = [];
-    int width = resolution.width.toInt();
-    int height = resolution.height.toInt();
-    double scale = (bottomRight.x - topLeft.x) / resolution.width;
-    if (width > bottomRight.x - topLeft.x) {
-      width = (bottomRight.x - topLeft.x).abs().toInt();
-      height = (bottomRight.y - topLeft.y).abs().toInt();
-      scale = 1;
-    }
-    for (int i = 0; i <= height; i++) {
-      List<Square> row = [];
-      int y = (topLeft.y.toInt() + i * scale).round();
-      for (int j = 0; j <= width; j++) {
-        int x = (topLeft.x.toInt() + j * scale).round();
-        row.add(map.getSquare(x, y));
-      }
-      squares.add(row);
-    }
-    return squares;
-  }
-
-  int get vision => _vision;
 }
