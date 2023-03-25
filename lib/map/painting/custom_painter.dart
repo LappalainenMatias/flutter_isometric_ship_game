2ab754f76f0bd65c2134f7b9b7e84a986316a8c5
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-03-25 16:01:06 +0200

    Add a faster drawing method
    40000 points are drawn in 20 ms

--- lib/map/painting/custom_painter.dart
+++ lib/map/painting/custom_painter.dart
@@ -1,131 +1,123 @@
+import 'dart:collection';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'dart:ui';
-
-import 'package:anki/map/square.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../../game.dart';
-import 'dart:math';
-import '../square_visibility.dart';
+import 'package:anki/map/map.dart';
 
 class MapPainter extends CustomPainter {
   Size maxResolution = const Size(301, 301);
-  final GameModel game;
+  final MapModel map;
   var rectPaint = Paint()
     ..color = const Color(0xff995588)
     ..style = PaintingStyle.fill;
 
-  MapPainter(this.game);
+  MapPainter(this.map);
 
   @override
   void paint(Canvas canvas, Size size) {
     Stopwatch start = Stopwatch()..start();
-    List<List<Color>> table = game.map.getMapSquares();
-    _paintSquares(canvas, table, size);
-    print("Painting took ${start.elapsedMilliseconds} ms");
+    _paintPoints(canvas, map.getGroundMatrix(), size);
+    print("Total paint: ${start.elapsedMilliseconds} ms");
   }
 
-  _paintSquares(Canvas canvas, List<List<Color>> table, size) {
-    int maxRows = game.map.vision > maxResolution.height
+  void _paintPoints(Canvas canvas, List matrix, Size size) {
+    Stopwatch start = Stopwatch()..start();
+    final width = size.width / matrix.length;
+    final height = size.height / matrix.first.length;
+    Map pointColors = {};
+
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[i].length; j++) {
+        final y = i * width;
+        final x = j * height;
+        Color color = Color.fromARGB(
+            matrix[i][j][3], matrix[i][j][0], matrix[i][j][1], matrix[i][j][2]);
+        pointColors.putIfAbsent(color, () => [x,y]).addAll([x,y]);
+      }
+    }
+    print("create: ${start.elapsedMilliseconds} ms");
+    start = Stopwatch()..start();
+    for (final color in pointColors.keys) {
+      canvas.drawRawPoints(
+        PointMode.points,
+        Float32List.fromList(pointColors[color]!),
+        rectPaint
+          ..blendMode = BlendMode.srcOver
+          ..strokeWidth = width + 1
+          ..color = color,
+      );
+    }
+    print("paint: ${start.elapsedMilliseconds} ms");
+  }
+
+  void _paintGroundTesting(Canvas canvas, List matrix, Size size) {
+    int maxRows = map.camera.height > maxResolution.height
         ? maxResolution.height.toInt()
-        : game.map.vision;
+        : map.camera.height;
     int x = 0;
     int y = 0;
     double scale = size.width / maxRows;
-    for (List<Color> row in table) {
-      for (Color color in row) {
+
+    final vertices = <Offset>[];
+    final colors = <Color>[];
+
+    for (final row in matrix) {
+      for (final color in row) {
+        final topLeftX = x * scale;
+        final topLeftY = y * scale;
+        final bottomRightX = x * scale + scale;
+        final bottomRightY = y * scale + scale;
+        vertices.addAll([
+          Offset(topLeftX, topLeftY),
+          Offset(topLeftX, bottomRightY),
+          Offset(bottomRightX, bottomRightY),
+          Offset(bottomRightX, topLeftY),
+        ]);
+        colors.addAll([
+          Color.fromARGB(color[3], color[0], color[1], color[2]),
+          Color.fromARGB(color[3], color[0], color[1], color[2]),
+          Color.fromARGB(color[3], color[0], color[1], color[2]),
+          Color.fromARGB(color[3], color[0], color[1], color[2]),
+        ]);
+        x++;
+      }
+      y++;
+      x = 0;
+    }
+
+    const vertexMode = VertexMode.triangles;
+    final indices = List.generate(vertices.length, (i) => i);
+    final verticesObject =
+        Vertices(vertexMode, vertices, colors: colors, indices: indices);
+    canvas.drawVertices(verticesObject, BlendMode.srcOver,
+        rectPaint..blendMode = BlendMode.srcOver);
+  }
+
+  _paintGroundRect(canvas, matrix, size) {
+    int maxRows = map.camera.height > maxResolution.height
+        ? maxResolution.height.toInt()
+        : map.camera.height;
+    int x = 0;
+    int y = 0;
+    double scale = size.width / maxRows;
+    for (List row in matrix) {
+      for (var colors in row) {
         double topLeftX = x * scale - 1; // -1 and +1 removes grid lines
         double topLeftY = y * scale - 1;
         double bottomRightX = x * scale + scale + 1;
         double bottomRightY = y * scale + scale + 1;
-        rectPaint.color = color;
-        final vertices = Vertices(VertexMode.triangles, [
-          Offset(topLeftX, topLeftY),
-          Offset(bottomRightX, topLeftY),
-          Offset(bottomRightX, bottomRightY),
-          Offset(topLeftX, topLeftY),
-          Offset(bottomRightX, bottomRightY),
-          Offset(topLeftX, bottomRightY),
-        ]);
-        canvas.drawVertices(vertices, BlendMode.src, rectPaint);
+        rectPaint.color =
+            Color.fromARGB(colors[3], colors[0], colors[1], colors[2]);
+        canvas.drawRect(
+            Rect.fromLTRB(topLeftX, topLeftY, bottomRightX, bottomRightY),
+            rectPaint);
         x++;
       }
       y++;
       x = 0;
     }
-  }
-
-  /// This is used for better performance
-  paintSquares(Canvas canvas, GameModel game, Size size) {
-    int maxRows = game.map.vision > maxResolution.height
-        ? maxResolution.height.toInt()
-        : game.map.vision;
-    int x = 0;
-    double y = 0;
-    double scale = size.width / maxRows;
-    double skip = game.map.vision > maxRows ? game.map.vision / maxRows : 1;
-    int startRow = game.map.player.getCoordinate().y - (game.map.vision / 2).ceil();
-    while (y < maxRows) {
-      int row = (startRow + y * skip).floor();
-      double topLeftX = x * scale - 1; // -1 and +1 removes grid lines
-      double topLeftY = y * scale - 1;
-      double bottomRightX = x * scale + scale + 1;
-      double bottomRightY = y * scale + scale + 1;
-      for (Square square in getSquares(row)) {
-        topLeftX = x * scale - 1; // -1 and +1 removes grid lines
-        topLeftY = y * scale - 1;
-        bottomRightX = x * scale + scale + 1;
-        bottomRightY = y * scale + scale + 1;
-        rectPaint.color = getSquareColor(square);
-        final vertices = Vertices(VertexMode.triangles, [
-          Offset(topLeftX, topLeftY),
-          Offset(bottomRightX, topLeftY),
-          Offset(bottomRightX, bottomRightY),
-          Offset(topLeftX, topLeftY),
-          Offset(bottomRightX, bottomRightY),
-          Offset(topLeftX, bottomRightY),
-        ]);
-        canvas.drawVertices(vertices, BlendMode.src, rectPaint);
-        x++;
-      }
-      y++;
-      x = 0;
-    }
-  }
-
-  Color getSquareColor(Square square) {
-    if (square.visibility == SquareVisibility.unseen) return Colors.black;
-    if (square.visibility == SquareVisibility.seen) return square.colorSeen;
-    return square.colorInView;
-  }
-
-  /// Resolution is the widgets resolution.
-  /// We use resolution so that we do not return unnecessary large amount of squares.
-  List<Square> getSquares(int column) {
-    int halfVision = (game.map.vision / 2).ceil();
-    Point playerCoordinate = game.map.player.getCoordinate();
-    List<Square> squares = _getSquaresWithMaxResolutionRow(
-      Point(playerCoordinate.x - halfVision, column),
-      Point(playerCoordinate.x + halfVision, column),
-    );
-    return squares;
-  }
-
-  /// returns all the squares which are in the are defined by top left and bottom right corner
-  List<Square> _getSquaresWithMaxResolutionRow(
-      Point topLeft, Point bottomRight) {
-    int width = maxResolution.width.toInt();
-    double scale = (bottomRight.x - topLeft.x) / maxResolution.width;
-    if (width > bottomRight.x - topLeft.x) {
-      width = (bottomRight.x - topLeft.x).abs().toInt();
-      scale = 1;
-    }
-    int y = (topLeft.y.toInt() + 1 * scale).round();
-    List<Square> row = [];
-    for (int j = 0; j <= width; j++) {
-      int x = (topLeft.x.toInt() + j * scale).round();
-      row.add(game.map.getSquare(x, y));
-    }
-    return row;
   }
 
   @override
