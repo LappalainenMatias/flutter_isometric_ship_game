d974de656a4b3e69ddb3934640db00da32f95d12
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-03-25 16:00:32 +0200

    Change player movement up direction
    The map is drawn from top left to bottom right but this should not affect the player movement up direction

--- lib/movement/player_mover.dart
+++ lib/movement/player_mover.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
-
 import 'package:anki/map/map.dart';
-
 import '../character/character.dart';
 import '../map/map_helper.dart';
 import '../map/square.dart';
@@ -37,21 +35,21 @@ class PlayerMover {
     int x = character.getCoordinate().x;
     int y = character.getCoordinate().y;
     if (angle > 337.5 || angle < 22.5) {
-      _move(x, y - 1, character); // Up
+      _move(x, y + 1, character); // Up
     } else if (angle > 22.5 && angle < 67.5) {
-      _move(x + 1, y - 1, character); // Up right
+      _move(x + 1, y + 1, character); // Up right
     } else if (angle > 67.5 && angle < 112.5) {
       _move(x + 1, y, character); // Right
     } else if (angle > 112.5 && angle < 157.5) {
-      _move(x + 1, y + 1, character); // Down right
+      _move(x + 1, y - 1, character); // Down right
     } else if (angle > 157.5 && angle < 202.5) {
-      _move(x, y + 1, character); // Down
+      _move(x, y - 1, character); // Down
     } else if (angle > 202.5 && angle < 247.5) {
-      _move(x - 1, y + 1, character); // Down left
+      _move(x - 1, y - 1, character); // Down left
     } else if (angle > 247.5 && angle < 292.5) {
       _move(x - 1, y, character); // Left
     } else if (angle > 292.5 && angle < 337.5) {
-      _move(x - 1, y - 1, character); // Up left
+      _move(x - 1, y + 1, character); // Up left
     }
   }
 }
