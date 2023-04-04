af9731108ab2d6694b15d431dc0de5334b4b054f
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-04-04 19:41:07 +0300

    Remove appbar
    Change zoom in and zoom out to bottom left corner

--- lib/main.dart
+++ lib/main.dart
@@ -58,12 +58,42 @@ class _MainScreenState extends State<MainScreen> {
     var game = Provider.of<GameModel>(context, listen: false);
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: buildAppBar(game),
       body: Stack(
         children: [
           Align(
               alignment: Alignment.bottomLeft,
               child: Board(width: size.width, height: size.height)),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    child: const Icon(
+                      Icons.zoom_in,
+                      color: Colors.white,
+                      size: 36.0,
+                    ),
+                    onTap: () {
+                      game.map.zoomIn();
+                    },
+                  ),
+                  GestureDetector(
+                    child: const Icon(
+                      Icons.zoom_out,
+                      color: Colors.white,
+                      size: 36.0,
+                    ),
+                    onTap: () {
+                      game.map.zoomOut();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
           Align(
             alignment: Alignment.bottomRight,
             child: Padding(
