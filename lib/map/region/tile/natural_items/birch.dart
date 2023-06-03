23a1ec329578fe1190018cc51e7d3c1ffc858019
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-03 13:39:37 +0300

    Simplify trees

--- lib/map/region/tile/natural_items/birch.dart
+++ lib/map/region/tile/natural_items/birch.dart
@@ -19,9 +19,8 @@ const Color foliageRight = Color.fromARGB(255, 8, 133, 38);
 IsoCoordinate offset = const IsoCoordinate(0, 0);
 
 /// Creates tree from cubes
-///   _______
-///  |______|
-///   |______|
+///  ________
+/// |________|
 /// |________|
 /// |________|
 ///    |__|
@@ -32,181 +31,44 @@ List birchPosAndCol(Tile tile) {
     Random().nextDouble() / 2,
   );
   int random = Random().nextInt(100);
-  if (random < 80) {
+  if (random < 95) {
     return _birch(tile);
-  } else if (random < 95) {
-    return _birchLowFoliage(tile);
-  } else {
-    return _randomTrunk(tile);
-  }
-}
-
-List _birchLowFoliage(Tile tile) {
-  var trunk = _randomTrunk(tile);
-  var foliage1 = _birchFoliageFirstLayer(tile);
-  var foliage2 = _birchFoliageSecondLayer(tile);
-  trunk[0].addAll(foliage1[0]);
-  trunk[1].addAll(foliage1[1]);
-  trunk[0].addAll(foliage2[0]);
-  trunk[1].addAll(foliage2[1]);
-  return trunk;
-}
-
-List _randomTrunk(Tile tile) {
-  if (Random().nextInt(2) < 1) {
-    return _birchTrunk2(tile);
   } else {
     return _birchTrunk(tile);
   }
 }
 
 List _birch(Tile tile) {
-  var trunk = _randomTrunk(tile);
+  var trunk = _birchTrunk(tile);
   var foliage1 = _birchFoliageFirstLayer(tile);
-  var foliage2 = _birchFoliageSecondLayer(tile);
-  var foliage3 = _birchFoliageThirdLayer(tile);
-  var foliage4 = _birchFoliageFourthLayer(tile);
   trunk[0].addAll(foliage1[0]);
   trunk[1].addAll(foliage1[1]);
-  trunk[0].addAll(foliage2[0]);
-  trunk[1].addAll(foliage2[1]);
-  trunk[0].addAll(foliage3[0]);
-  trunk[1].addAll(foliage3[1]);
-  trunk[0].addAll(foliage4[0]);
-  trunk[1].addAll(foliage4[1]);
   return trunk;
 }
 
 List _birchFoliageFirstLayer(Tile tile) {
   return createCube(
     tile.coordinate,
-    tile.height + 3.80,
+    tile.height + 2.00,
     foliageTop.value,
     foliageLeft.value,
     foliageRight.value,
     widthScale: 1.25,
+    heightScale: 2.25 * (Random().nextDouble() + 0.5),
     offset: offset,
   );
 }
 
-List _birchFoliageSecondLayer(Tile tile) {
-  return createCube(tile.coordinate, tile.height + 2.7, foliageTop.value,
-      foliageLeft.value, foliageRight.value,
-      widthScale: 0.7,
-      heightScale: 1.0,
-      offset: offset + const IsoCoordinate(1.0, 0.1));
-}
-
-List _birchFoliageThirdLayer(Tile tile) {
-  return createCube(tile.coordinate, tile.height + 3.0, foliageTop.value,
-      foliageLeft.value, foliageRight.value,
-      widthScale: 0.75,
-      heightScale: 1.2,
-      offset: offset + const IsoCoordinate(-1.0, 0.0));
-}
-
-List _birchFoliageFourthLayer(Tile tile) {
-  return createCube(tile.coordinate, tile.height + 3.0, foliageTop.value,
-      foliageLeft.value, foliageRight.value,
-      widthScale: 0.5, heightScale: 0.7, offset: offset);
-}
 
 List _birchTrunk(Tile tile) {
-  List trunkStart = createCube(
+  return createCube(
     tile.coordinate,
     tile.height + 1.25,
     trunkTop.value,
     trunkLeft.value,
     trunkRight.value,
     widthScale: 0.25,
-    heightScale: 1.5,
+    heightScale: 1.75 * (Random().nextDouble() + 0.5),
     offset: offset,
   );
-  List blackLine = createCube(
-    tile.coordinate,
-    tile.height + 2.6,
-    lineTop.value,
-    lineLeft.value,
-    lineRight.value,
-    widthScale: 0.25,
-    heightScale: 0.2,
-    offset: offset,
-  );
-  List trunkEnd = createCube(
-    tile.coordinate,
-    tile.height + 2.8,
-    trunkTop.value,
-    trunkLeft.value,
-    trunkRight.value,
-    widthScale: 0.25,
-    heightScale: 1.5,
-    offset: offset,
-  );
-  trunkStart[0].addAll(blackLine[0]);
-  trunkStart[1].addAll(blackLine[1]);
-  trunkStart[0].addAll(trunkEnd[0]);
-  trunkStart[1].addAll(trunkEnd[1]);
-  return trunkStart;
-}
-
-List _birchTrunk2(Tile tile) {
-  List trunkStart = createCube(
-    tile.coordinate,
-    tile.height + 1.25,
-    trunkTop.value,
-    trunkLeft.value,
-    trunkRight.value,
-    widthScale: 0.25,
-    heightScale: 0.8,
-    offset: offset,
-  );
-  List blackLine = createCube(
-    tile.coordinate,
-    tile.height + 2.0,
-    lineTop.value,
-    lineLeft.value,
-    lineRight.value,
-    widthScale: 0.25,
-    heightScale: 0.2,
-    offset: offset,
-  );
-  List trunkMiddle = createCube(
-    tile.coordinate,
-    tile.height + 2.2,
-    trunkTop.value,
-    trunkLeft.value,
-    trunkRight.value,
-    widthScale: 0.25,
-    heightScale: 0.40,
-    offset: offset,
-  );
-  List blackLineTop = createCube(
-    tile.coordinate,
-    tile.height + 2.4,
-    lineTop.value,
-    lineLeft.value,
-    lineRight.value,
-    widthScale: 0.25,
-    heightScale: 0.2,
-    offset: offset,
-  );
-  List trunkEnd = createCube(
-    tile.coordinate,
-    tile.height + 2.6,
-    trunkTop.value,
-    trunkLeft.value,
-    trunkRight.value,
-    widthScale: 0.25,
-    heightScale: 1.5,
-    offset: offset,
-  );
-  trunkStart[0].addAll(blackLine[0]);
-  trunkStart[1].addAll(blackLine[1]);
-  trunkStart[0].addAll(trunkMiddle[0]);
-  trunkStart[1].addAll(trunkMiddle[1]);
-  trunkStart[0].addAll(blackLineTop[0]);
-  trunkStart[1].addAll(blackLineTop[1]);
-  trunkStart[0].addAll(trunkEnd[0]);
-  trunkStart[1].addAll(trunkEnd[1]);
-  return trunkStart;
 }
--- lib/map/region/tile/natural_items/spruce.dart
+++ lib/map/region/tile/natural_items/spruce.dart
@@ -15,10 +15,9 @@ const Color foliageRight = Color.fromARGB(255, 6, 101, 28);
 IsoCoordinate offset = const IsoCoordinate(0, 0);
 
 /// Creates tree from cubes
-///     __
-///    |__|
-///   |____|
-///  |______|
+///  ________
+/// |________|
+/// |________|
 /// |________|
 ///    |__|
 ///    |__|
@@ -28,48 +27,18 @@ List sprucePosAndCol(Tile tile) {
     Random().nextDouble() / 2,
   );
   int random = Random().nextInt(100);
-  if (random < 80) {
+  if (random < 95) {
     return _spruce(tile);
-  } else if (random < 95) {
-    return _spruceLowFoliage(tile);
   } else {
-    return _spruceWithoutFoliage(tile);
+    return _spruceTrunk(tile);
   }
 }
 
 List _spruce(Tile tile) {
   var trunk = _spruceTrunk(tile);
   var foliage1 = _spruceFoliageFirstLayer(tile);
-  var foliage2 = _spruceFoliageSecondLayer(tile);
-  var foliage3 = _spruceFoliageThirdLayer(tile);
-  var foliage4 = _spruceFoliageFourthLayer(tile);
   trunk[0].addAll(foliage1[0]);
   trunk[1].addAll(foliage1[1]);
-  trunk[0].addAll(foliage2[0]);
-  trunk[1].addAll(foliage2[1]);
-  trunk[0].addAll(foliage3[0]);
-  trunk[1].addAll(foliage3[1]);
-  trunk[0].addAll(foliage4[0]);
-  trunk[1].addAll(foliage4[1]);
-  return trunk;
-}
-
-List _spruceWithoutFoliage(Tile tile) {
-  var trunk = _spruceTrunk(tile);
-  return trunk;
-}
-
-List _spruceLowFoliage(Tile tile) {
-  var trunk = _spruceTrunk(tile);
-  var foliage2 = _spruceFoliageSecondLayer(tile);
-  var foliage3 = _spruceFoliageThirdLayer(tile);
-  var foliage4 = _spruceFoliageFourthLayer(tile);
-  trunk[0].addAll(foliage2[0]);
-  trunk[1].addAll(foliage2[1]);
-  trunk[0].addAll(foliage3[0]);
-  trunk[1].addAll(foliage3[1]);
-  trunk[0].addAll(foliage4[0]);
-  trunk[1].addAll(foliage4[1]);
   return trunk;
 }
 
@@ -80,44 +49,7 @@ List _spruceFoliageFirstLayer(Tile tile) {
     foliageTop.value,
     foliageLeft.value,
     foliageRight.value,
-    offset: offset,
-  );
-}
-
-List _spruceFoliageSecondLayer(Tile tile) {
-  return createCube(
-    tile.coordinate,
-    tile.height + 3.1,
-    foliageTop.value,
-    foliageLeft.value,
-    foliageRight.value,
-    widthScale: 0.7,
-    offset: offset,
-  );
-}
-
-List _spruceFoliageThirdLayer(Tile tile) {
-  return createCube(
-    tile.coordinate,
-    tile.height + 4.2,
-    foliageTop.value,
-    foliageLeft.value,
-    foliageRight.value,
-    widthScale: 0.5,
-    heightScale: 0.8,
-    offset: offset,
-  );
-}
-
-List _spruceFoliageFourthLayer(Tile tile) {
-  return createCube(
-    tile.coordinate,
-    tile.height + 5.1,
-    foliageTop.value,
-    foliageLeft.value,
-    foliageRight.value,
-    widthScale: 0.3,
-    heightScale: 0.8,
+    heightScale: 2.5 * (Random().nextDouble() + 0.5),
     offset: offset,
   );
 }
@@ -130,7 +62,7 @@ List _spruceTrunk(Tile tile) {
     trunkLeft.value,
     trunkRight.value,
     widthScale: 0.25,
-    heightScale: 3.0,
+    heightScale: 2.0 * (Random().nextDouble() + 0.5),
     offset: offset,
   );
 }
