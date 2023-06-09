f51a8c092d90ec916e643f87750fb8ed55829af4
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-10 00:09:23 +0300

    Update tests

--- test/map/iso_coordinate_test.dart
+++ test/map/iso_coordinate_test.dart
@@ -6,29 +6,15 @@ void main() {
     IsoCoordinate topLeft = const IsoCoordinate(0, 0);
     IsoCoordinate bottomRight = const IsoCoordinate(128, 128);
     IsoCoordinate center = topLeft.center(bottomRight);
-    expect(center.x, 64);
-    expect(center.y, 64);
+    expect(center.isoX, 64);
+    expect(center.isoY, 64);
   });
 
   test("Find iso coordinate center", () {
     IsoCoordinate topLeft = const IsoCoordinate(-2000, -1000);
     IsoCoordinate bottomRight = const IsoCoordinate(1000, 3000);
     IsoCoordinate center = topLeft.center(bottomRight);
-    expect(center.x, -500);
-    expect(center.y, 1000);
-  });
-
-  test("Euclidean isometric distance", () {
-    IsoCoordinate topLeft = const IsoCoordinate(0, 0);
-    IsoCoordinate bottomRight = const IsoCoordinate(1, 1);
-    double distance = topLeft.euclideanIsoDistance(bottomRight);
-    expect(distance, 2);
-  });
-
-  test("Euclidean isometric distance", () {
-    IsoCoordinate topLeft = const IsoCoordinate(-10, 5); // -30, -5
-    IsoCoordinate bottomRight = const IsoCoordinate(5, 4); // 2, 9
-    double distance = topLeft.euclideanIsoDistance(bottomRight);
-    expect(distance, 32 + 14);
+    expect(center.isoX, -500);
+    expect(center.isoY, 1000);
   });
 }
--- test/region_generator_test.dart
+++ test/region_generator_test.dart
@@ -3,10 +3,16 @@ import 'package:anki/map/region/region_manager.dart';
 import 'package:test/test.dart';
 
 void main() {
+  /// Results (ms):
+  /// 538/537/538
+  /// 542/523/540
+  /// 510/623/507/512
+  /// 452/428/414/441 SimplexNoise
+  /// 357/354/357/357 OpenSimplexNoise
   test('test', () {
     Stopwatch stopwatch = Stopwatch()..start();
     RegionCreator regionCreator = RegionCreator();
-    regionCreator.create(const IsoCoordinate(0, 0), 128, 128, 0, 0);
+    regionCreator.create(const IsoCoordinate(0, 0), 256, 256, 0, 0);
     print("test took ${stopwatch.elapsedMilliseconds} ms");
   });
 }
