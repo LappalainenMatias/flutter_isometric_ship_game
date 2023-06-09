93ea732902a60c9c17b096ab1bb86e6b53b84887
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-10 00:11:22 +0300

    Simplify camera mover

--- lib/map/camera/camera_mover.dart
+++ lib/map/camera/camera_mover.dart
@@ -3,11 +3,7 @@ import 'package:anki/map/iso_coordinate.dart';
 import 'camera.dart';
 
 class CameraMover {
-  double movementDistance = 0.0020;
-
-  void _move(double newX, double newY, Camera camera) {
-    camera.centralize(IsoCoordinate(newX, newY));
-  }
+  double movementDistance = 3.0;
 
   /// Moves the camera in the direction indicated by the origin (0, 0) and (x, y)
   /// (0, 1) = up, (-1, 0) = left. Notice that the map is isometric which means that
@@ -17,39 +13,9 @@ class CameraMover {
     double joyStickY,
     Camera camera,
   ) {
-    double angle = (atan2(joyStickX, joyStickY) * (180 / pi) + 360) % 360;
-    double x = camera.center.x;
-    double y = camera.center.y;
-    double md = movementDistance * camera.width;
-    md = md * _euclideanDistance(0, 0, joyStickX, joyStickY);
-    if (angle > 337.5 || angle < 22.5) {
-      // Up
-      _move(x + md, y + md, camera);
-    } else if (angle > 22.5 && angle < 67.5) {
-      // Up right
-      _move(x + md, y, camera);
-    } else if (angle > 67.5 && angle < 112.5) {
-      // Right
-      _move(x + md, y - md, camera);
-    } else if (angle > 112.5 && angle < 157.5) {
-      // Down right
-      _move(x, y - md, camera);
-    } else if (angle > 157.5 && angle < 202.5) {
-      // Down
-      _move(x - md, y - md, camera);
-    } else if (angle > 202.5 && angle < 247.5) {
-      // Down left
-      _move(x - md, y, camera);
-    } else if (angle > 247.5 && angle < 292.5) {
-      // Left
-      _move(x - md, y + md, camera);
-    } else if (angle > 292.5 && angle < 337.5) {
-      // Up left
-      _move(x, y + md, camera);
-    }
-  }
-
-  double _euclideanDistance(double x1, double y1, double x2, double y2) {
-    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    camera.center = IsoCoordinate.fromIso(
+      camera.center.isoX + joyStickX * movementDistance,
+      camera.center.isoY + joyStickY * movementDistance,
+    );
   }
 }
