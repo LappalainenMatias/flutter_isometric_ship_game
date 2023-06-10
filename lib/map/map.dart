7029a6230b07529fd0664168d1c1524033f7e349
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-10 22:40:56 +0300

    Add data for statistics

--- lib/map/map.dart
+++ lib/map/map.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:anki/map/iso_coordinate.dart';
 import 'package:anki/map/region/region_manager.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,9 +8,15 @@ import 'camera/camera.dart';
 class MapModel extends ChangeNotifier {
   final RegionManager _regionManager = RegionManager();
   final Camera _camera = Camera();
+  int _verticesCount = 0;
+  int _regionCount = 0;
 
-  Map<String, List<Vertices>> getVerticesInView() {
-    return _regionManager.getVertices(_camera.topLeft, _camera.bottomRight);
+  Map<String, dynamic> getVerticesInView() {
+    var vertices =
+        _regionManager.getVertices(_camera.topLeft, _camera.bottomRight);
+    _verticesCount = vertices["verticesCount"];
+    _regionCount = vertices["regionCount"];
+    return vertices;
   }
 
   /// Moves the camera in the direction indicated by the origin (0, 0) and (x, y)
@@ -20,11 +25,23 @@ class MapModel extends ChangeNotifier {
     _camera.move(joyStickX, joyStickY);
   }
 
+  int get verticesCount => _verticesCount;
+
+  int get regionCount => _regionCount;
+
   double get width => _camera.width;
 
   double get height => _camera.height;
 
+  IsoCoordinate get topLeft => _camera.topLeft;
+
+  IsoCoordinate get bottomRight => _camera.bottomRight;
+
   IsoCoordinate get center => _camera.center;
+
+  void setAspectRatio(double ratio) {
+    _camera.aspectRatio = ratio;
+  }
 
   void setZoomLevel(double level) {
     _camera.setZoomLevel(level);
