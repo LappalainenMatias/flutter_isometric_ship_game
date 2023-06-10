d2da9c6de1a1998aedc5e1a7118c8fa09aa29d78
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-10 22:39:39 +0300

    Fix camera corners issue
    Topleft and bottomright were not at the topleft and bottomright of the screen

--- lib/map/camera/camera.dart
+++ lib/map/camera/camera.dart
@@ -2,68 +2,34 @@ import 'package:anki/map/iso_coordinate.dart';
 import 'camera_mover.dart';
 
 class Camera {
-  late IsoCoordinate topLeft;
-  late IsoCoordinate bottomRight;
+  late IsoCoordinate center;
   final CameraMover _cameraMover = CameraMover();
-  double _zoomLevel = 1.0;
-  final double _minWidth = 16;
-  final double _maxWidth = 600;
+  double _zoomLevel = 0.5;
+  final double _minWidth = 32;
+  final double _maxWidth = 1024;
+  double _aspectRatio = 1.0;
 
-  Camera({IsoCoordinate center = const IsoCoordinate(0, 0)}) {
-    topLeft = IsoCoordinate(
-      center.x - 128,
-      center.y + 128,
-    );
-    bottomRight = IsoCoordinate(
-      center.x + 128,
-      center.y - 128,
-    );
-    _updateCamera();
-  }
+  Camera({this.center = const IsoCoordinate(0, 0)});
 
   void move(double joyStickX, double joyStickY) {
     _cameraMover.joyStickIsometricMovement(joyStickX, joyStickY, this);
   }
 
-  void centralize(IsoCoordinate coordinate) {
-    double width = (topLeft.x - bottomRight.x).abs();
-    double height = (topLeft.y - bottomRight.y).abs();
-    topLeft =
-        IsoCoordinate(coordinate.x - width / 2, coordinate.y + height / 2);
-    bottomRight =
-        IsoCoordinate(coordinate.x + width / 2, coordinate.y - height / 2);
+  set aspectRatio(double ratio) {
+    _aspectRatio = ratio;
   }
 
-  double get width => (topLeft.x - bottomRight.x).abs();
+  double get width => _zoomLevel * _maxWidth + _minWidth;
 
-  double get height => (topLeft.y - bottomRight.y).abs();
+  double get height => _zoomLevel * (_maxWidth / _aspectRatio) + _minWidth;
 
-  IsoCoordinate get center {
-    return IsoCoordinate(
-      (topLeft.x + bottomRight.x) / 2.0,
-      (topLeft.y + bottomRight.y) / 2.0,
-    );
-  }
+  IsoCoordinate get bottomRight =>
+      IsoCoordinate.fromIso(center.isoX + width / 2, center.isoY - height / 2);
 
-  void setZoomLevel(double zoom) {
-    _zoomLevel = zoom;
-    _updateCamera();
-  }
+  IsoCoordinate get topLeft =>
+      IsoCoordinate.fromIso(center.isoX - width / 2, center.isoY + height / 2);
 
-  void _updateCamera() {
-    double centerX = (topLeft.x + bottomRight.x) / 2.0;
-    double centerY = (topLeft.y + bottomRight.y) / 2.0;
-    double newWidth = _maxWidth * _zoomLevel + _minWidth;
-    double newHeight = _maxWidth * _zoomLevel + _minWidth;
-
-    topLeft = IsoCoordinate(
-      (centerX - newWidth / 2.0),
-      (centerY - newHeight / 2.0),
-    );
-    bottomRight = IsoCoordinate(
-      (centerX + newWidth / 2.0),
-      (centerY + newHeight / 2.0),
-    );
-    centralize(center);
+  void setZoomLevel(double zoomLevel) {
+    _zoomLevel = zoomLevel;
   }
 }
