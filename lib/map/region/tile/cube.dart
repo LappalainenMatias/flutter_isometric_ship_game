d66c967c3654b06d9e2315deb29d615f140c5383
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-06-10 22:33:01 +0300

    Remove unnecessary color mixing

--- lib/map/region/tile/cube.dart
+++ lib/map/region/tile/cube.dart
@@ -1,33 +1,39 @@
 import 'dart:ui';
-
+import 'dart:math';
 import 'package:anki/map/iso_coordinate.dart';
 
 const Color blueColor = Color(0xFF012E8F);
+
 /// Creates a list of positions and colors
 /// Isometric cube has 7 corners and 3 visible sides.
 /// From the 7 corners we can create 6 triangles that make up the cube.
 /// The scale makes the cubes thinner/wider/shorter/taller
 /// offset can be used to reduce symmetry by moving the cube slightly
 List createCube(
-  IsoCoordinate coordinate,
+  Point<double> coordinate,
   double tileHeight,
   int colorTop,
   int colorLeft,
   int colorRight, {
   double heightScale = 1,
   double widthScale = 1,
-  IsoCoordinate offset = const IsoCoordinate(0, 0),
+  IsoCoordinate offset = const IsoCoordinate.fromIso(0, 0),
 }) {
   if (tileHeight < 0) {
-    double depthPercentage = (tileHeight / 10).abs() + 0.3;
-    if (depthPercentage > 1) depthPercentage = 1;
-    colorTop = mix(Color(colorTop), blueColor, depthPercentage).value;
-    colorLeft = mix(Color(colorLeft), blueColor, depthPercentage).value;
-    colorRight = mix(Color(colorRight), blueColor, depthPercentage).value;
+    double depthPercentage = (tileHeight / 5).abs();
+    if (depthPercentage > 1) {
+      colorTop = blueColor.value;
+      colorLeft = blueColor.value;
+      colorRight = blueColor.value;
+    } else {
+      colorTop = mix(Color(colorTop), blueColor, depthPercentage).value;
+      colorLeft = mix(Color(colorLeft), blueColor, depthPercentage).value;
+      colorRight = mix(Color(colorRight), blueColor, depthPercentage).value;
+    }
   }
-  double x = coordinate.x.toDouble() + tileHeight;
-  double y = coordinate.y.toDouble() + tileHeight;
-  final cenBot = IsoCoordinate(x, y) + offset;
+  final cenBot =
+      IsoCoordinate(coordinate.x + tileHeight, coordinate.y + tileHeight) +
+          offset;
   final cenCen = cenBot + IsoCoordinate(heightScale, heightScale);
   final lefBot = cenBot + IsoCoordinate(0, widthScale);
   final lefTop = cenCen + IsoCoordinate(0, widthScale);
