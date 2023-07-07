9e202c5a788c20c8ba791e9ae56a3e1617afe427
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-07-07 17:14:17 +0300

    Refactor noise to own class

--- lib/map/region/region_creator.dart
+++ lib/map/region/region_creator.dart
@@ -2,23 +2,17 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'package:anki/map/region/tile/tile.dart';
 import 'package:anki/map/region/tile/tile_creator.dart';
-import 'package:open_simplex_noise/open_simplex_noise.dart';
 import '../../utils/iso_coordinate.dart';
+import '../../utils/noise.dart';
 
 /// We use seperate class for creating the region data because this class
 /// does not use dart:ui and because of that it can be run concurrently
 class RegionCreator {
-  late OpenSimplexNoise _elevationNoise;
-  late OpenSimplexNoise _moistureNoise;
-
-  RegionCreator([int seed = 1]) {
-    _elevationNoise = OpenSimplexNoise(seed + 1);
-    _moistureNoise = OpenSimplexNoise(seed + 2);
-  }
+  final _noise = Noise();
 
   RegionDTO create(IsoCoordinate regionCoordinate, int width, int height,
       int startX, int startY) {
-    var noises = _createNoises(width, height, startX, startY);
+    var noises = _noise.create(width, height, startX, startY);
     final elevationNoise = noises[0];
     final moistureNoise = noises[1];
     List<Tile> tiles = [];
@@ -54,47 +48,6 @@ class RegionCreator {
       Float32List.fromList(underWaterPositions),
       Int32List.fromList(underWaterColors),
     );
-  }
-
-  /// Increasing frequency adds details
-  List<List<List<double>>> _createNoises(
-    int width,
-    int height,
-    int startX,
-    int startY,
-  ) {
-    List<List<double>> elevationMap = _fixedSizeList(width, height);
-    List<List<double>> moistureMap = _fixedSizeList(width, height);
-    for (int x = 0; x < width; x++) {
-      var rowElevation = elevationMap[x];
-      var rowMoisture = moistureMap[x];
-      for (int y = 0; y < height; y++) {
-        final x1 = (startX + x) * 0.006;
-        final y1 = (startY + y) * 0.006;
-        final x2 = (startX + x) * 0.016;
-        final y2 = (startY + y) * 0.016;
-        final x3 = (startX + x) * 0.048;
-        final y3 = (startY + y) * 0.048;
-        double elevation = _elevationNoise.eval2D(x1, y1) +
-            0.5 * _elevationNoise.eval2D(x2, y2) +
-            0.25 * _elevationNoise.eval2D(x3, y3);
-        double moisture = _moistureNoise.eval2D(x1, y1) +
-            0.5 * _moistureNoise.eval2D(x2, y2) +
-            0.25 * _moistureNoise.eval2D(x3, y3);
-        rowElevation[y] = elevation - 0.15;
-        rowMoisture[y] = moisture;
-      }
-    }
-    return [elevationMap, moistureMap];
-  }
-
-  List<List<double>> _fixedSizeList(int width, int height) {
-    List<List<double>> map = List.generate(
-      width,
-      (i) => List.generate(height, (i) => 0, growable: false),
-      growable: false,
-    );
-    return map;
   }
 }
 
---
+++ lib/utils/noise.dart
@@ -0,0 +1,53 @@
+import 'package:open_simplex_noise/open_simplex_noise.dart';
+
+class Noise {
+  late OpenSimplexNoise _elevationNoise1;
+  late OpenSimplexNoise _elevationNoise2;
+  late OpenSimplexNoise _elevationNoise3;
+  late OpenSimplexNoise _moistureNoise1;
+  late OpenSimplexNoise _moistureNoise2;
+  late OpenSimplexNoise _moistureNoise3;
+
+  Noise([int seed = 1]) {
+    _elevationNoise1 = OpenSimplexNoise(seed + 1);
+    _elevationNoise2 = OpenSimplexNoise(seed + 2);
+    _elevationNoise3 = OpenSimplexNoise(seed + 3);
+    _moistureNoise1 = OpenSimplexNoise(seed + 4);
+    _moistureNoise2 = OpenSimplexNoise(seed + 5);
+    _moistureNoise3 = OpenSimplexNoise(seed + 6);
+  }
+
+  /// Increasing frequency adds details
+  List<List<List<double>>> create(
+      int width, int height, int startX, int startY) {
+    List<List<double>> elevationMap = _fixedSizeList(width, height);
+    List<List<double>> moistureMap = _fixedSizeList(width, height);
+    for (int x = 0; x < width; x++) {
+      var rowElevation = elevationMap[x];
+      var rowMoisture = moistureMap[x];
+      for (int y = 0; y < height; y++) {
+        var i = (startX + x).toDouble();
+        var j = (startY + y).toDouble();
+        double elevation = _elevationNoise1.eval2D(i * 0.006, j * 0.006) +
+            0.5 * _elevationNoise2.eval2D(i * 0.016, j * 0.016) +
+            0.25 * _elevationNoise3.eval2D(i * 0.048, j * 0.048);
+        double moisture = _moistureNoise1.eval2D(i * 0.006, j * 0.006) +
+            0.5 * _moistureNoise2.eval2D(i * 0.016, j * 0.016) +
+            0.25 * _moistureNoise3.eval2D(i * 0.048, j * 0.048);
+        rowElevation[y] =
+            ((elevation - 0.35) * 20).roundToDouble(); // Increase height differences and water
+        rowMoisture[y] = moisture;
+      }
+    }
+    return [elevationMap, moistureMap];
+  }
+
+  List<List<double>> _fixedSizeList(int width, int height) {
+    List<List<double>> map = List.generate(
+      width,
+      (i) => List.generate(height, (i) => 0, growable: false),
+      growable: false,
+    );
+    return map;
+  }
+}
