e3563e194fe1d183f80f0d065a756d246fe82c40
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-10-05 20:20:23 +0300

    Add comments to constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,9 @@
 /// Region is this amount of tiles wide.
-/// This needs to be power of two because it makes lowering the level of detail easier.
+/// Larger regions are faster to render than multiple small regions.
+/// The data that comes from webworkers is decoded currenlty on the main thread, so it can cause
+/// lag if the region size is too large. Also if the region contains dynamic game objects,
+/// the painters algorithm and collision detection is slower for larger regions.
 const int regionSideWidth = 32;
 
 /// Amount of regions the game can have before it starts removing regions.
-const int maxRegionCount = 1000000;
+const int maxRegionCount = 100;
