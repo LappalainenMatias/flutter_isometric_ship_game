ed20b8cdb642b42848e4ba1de33895049e7096f9
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2023-11-25 19:26:27 +0200

    Add socket variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,4 +6,10 @@
 const int regionSideWidth = 16;
 
 /// Amount of regions the game can have before it starts removing regions.
-const int maxRegionCount = 1;
+const int maxRegionCount = 1000;
+
+/// To run locally
+/// 1. use localSocket ('ws://localhost:8080/ws')
+/// 2. run 'dart_frog dev' in the backend project
+const String socket = 'wss://game-backend-7myko4scwa-ew.a.run.app/ws';
+const String localSocket = 'ws://localhost:8080/ws';
